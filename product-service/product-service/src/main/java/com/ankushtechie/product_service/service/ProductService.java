0804d3ad9dea1aab8a1ad4ae619d46aab083356d
ankushga <ankushga@tscti.com>
2024-09-20 03:02:36 -0700

    Added Product Service

---
+++ product-service/product-service/src/main/java/com/ankushtechie/product_service/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.ankushtechie.product_service.service;
+
+import com.ankushtechie.product_service.dto.ProductRequest;
+import com.ankushtechie.product_service.dto.ProductResponse;
+import com.ankushtechie.product_service.model.Product;
+import com.ankushtechie.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest){
+        Product product=Product.builder()
+                .name(productRequest.getName())
+                .price(productRequest.getPrice())
+                .description(productRequest.getDescription())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product Details {}",product);
+        log.info("Product {} is saved",product.getId());
+
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products=productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
